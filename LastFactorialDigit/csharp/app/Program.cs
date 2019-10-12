91d417edf218bcb169bc33427c8b1e9c24a5f6e4
Peter K. Moss <peter@moss.dk>
2019-10-12 09:16:10 +0200

    Last Factorial Digit

---
+++ LastFactorialDigit/csharp/app/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            for (var i = 0; i < lines ; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                var res = 1;
+                for (var j = input ; j > 1; j--)
+                {
+                    res *= j;
+                }
+                Console.WriteLine(res.ToString().Last());
+            }
+        }
+    }
+}
