73db9c278cdb28fde66960f17e27569e6abf0535
Peter K. Moss <peter@moss.dk>
2019-10-12 14:23:21 +0200

    Detailed Differences

---
+++ DetailedDifferences/csharp/app/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cases = int.Parse(Console.ReadLine());
+            for (var i = 0; i < cases; i++)
+            {
+                var first = Console.ReadLine();
+                var second = Console.ReadLine();
+
+                Console.WriteLine(first);
+                Console.WriteLine(second);
+
+                for (var j = 0; j < first.Length; j++)
+                {
+                    if (first[j] == second[j])
+                        Console.Write(".");
+                    else
+                        Console.Write("*");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
