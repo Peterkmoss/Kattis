12f25e6f28f00507dbea9d18bf0c556a47000a96
Peter K. Moss <peter@moss.dk>
2019-10-12 08:00:30 +0200

    NastyHacks

---
+++ NastyHacks/csharp/app/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            for (var i = 0; i < lines ; i++)
+            {
+                var input = Console.ReadLine().Split(" ");
+                var r = int.Parse(input[0]);
+                var e = int.Parse(input[1]);
+                var c = int.Parse(input[2]);
+
+                if (e - r > c)
+                    Console.WriteLine("advertise");
+                else if (e - r == c)
+                    Console.WriteLine("does not matter");
+                else if (e - r < c)
+                    Console.WriteLine("do not advertise");
+            }
+        }
+    }
+}
