b5148c3718a59ac97daa52c5d750dc2233aa79a3
Peter K. Moss <peter@moss.dk>
2019-10-12 09:40:31 +0200

    IsItHalloween.com

---
+++ IsItHalloween.com/csharp/app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if (input == "OCT 31" || input == "DEC 25")
+                Console.WriteLine("yup");
+            else 
+                Console.WriteLine("nope");
+        }
+    }
+}
