c922f76d8cafa99bf1b1446a1ec17a074da58932
Peter K. Moss <peter@moss.dk>
2019-10-12 10:06:18 +0200

    HeartRate

---
+++ HeartRate/csharp/app/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            for (var i = 0 ; i < lines; i++)
+            {
+                var input = Console.ReadLine().Split(" ");
+                var b = double.Parse(input[0]);
+                var p = double.Parse(input[1]);
+                var abpm = 60.0 / p;
+                var abpmb = abpm * b;
+                Console.WriteLine((abpmb - abpm) + " " + abpmb + " " + (abpmb + abpm));
+            }
+        }
+    }
+}
