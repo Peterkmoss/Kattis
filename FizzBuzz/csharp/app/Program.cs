5a030904e9ad2d6e0cb4835d4957e4fd2027ae64
Peter K. Moss <peter@moss.dk>
2019-10-12 13:36:28 +0200

    FizzBuzz

---
+++ FizzBuzz/csharp/app/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ");
+            var x = int.Parse(input[0]);
+            var y = int.Parse(input[1]);
+            var n = int.Parse(input[2]);
+            for (var i = 1; i <= n ; i++)
+            {
+                if (i % x != 0 && i % y != 0)
+                {
+                    Console.WriteLine(i);
+                    continue;
+                }
+                if (i % x == 0)
+                    Console.Write("Fizz");
+                if (i % y == 0)
+                    Console.Write("Buzz");
+                Console.WriteLine();                
+            }
+        }
+    }
+}
