c408f38fa5e902f1b0ff1342dad74fbfc7b4f81e
Peter K. Moss <peter@moss.dk>
2019-10-12 09:34:06 +0200

    Ladder

---
+++ Ladder/csharp/app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Split(" ");
+            var h = int.Parse(line[0]);
+            var v = (int.Parse(line[1]) * Math.PI)/180;
+
+            Console.WriteLine(Math.Ceiling(h / Math.Sin(v)));
+        }
+    }
+}
