9eb03227738df49442dcf89801adc0ce431729d1
Peter K. Moss <peter@moss.dk>
2019-10-12 09:42:52 +0200

    Hissing Microphone

---
+++ Hissing Microphone/csharp/app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if (input.Contains("ss"))
+                Console.WriteLine("hiss");
+            else
+                Console.WriteLine("no hiss");
+        }
+    }
+}
