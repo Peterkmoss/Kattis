82d703d7a213963ffbe3ddfeba9177d7df54d6e1
Peter K. Moss <peter@moss.dk>
2019-10-12 13:29:38 +0200

    Grass Seed Inc.

---
+++ GrassSeedInc/csharp/app/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cost = float.Parse(Console.ReadLine());
+            var lawns = int.Parse(Console.ReadLine());
+            var res = 0.0;
+            for (var i = 0 ; i < lawns ; i++)
+            {
+                var lawn = Console.ReadLine().Split(" ");
+                var w = float.Parse(lawn[0]);
+                var l = float.Parse(lawn[1]);
+                res += w*l*cost;
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
