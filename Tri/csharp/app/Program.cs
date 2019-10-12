f3c4852adadc22af077db64a5be52ab7b56606a2
Peter K. Moss <peter@moss.dk>
2019-10-12 16:43:56 +0200

    Tri

---
+++ Tri/csharp/app/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ");
+            var x = int.Parse(input[0]);
+            var y = int.Parse(input[1]);
+            var z = int.Parse(input[2]);
+
+            if (x + y == z)
+                Console.WriteLine(x + "+" + y + "=" + z);
+            else if (x - y == z)
+                Console.WriteLine(x + "-" + y + "=" + z);
+            else if (x / y == z)
+                Console.WriteLine(x + "/" + y + "=" + z);
+            else if (x * y == z)
+                Console.WriteLine(x + "*" + y + "=" + z);
+            else if (x == y + z)
+                Console.WriteLine(x + "=" + y + "+" + z);
+            else if (x == y - z)
+                Console.WriteLine(x + "=" + y + "-" + z);
+            else if (x == y / z)
+                Console.WriteLine(x + "=" + y + "/" + z);
+            else if (x == y * z)
+                Console.WriteLine(x + "=" + y + "*" + z);
+        }
+    }
+}
