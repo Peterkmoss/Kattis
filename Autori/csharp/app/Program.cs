cd147cd32c07bf826ca47c7845d98a50faac984d
Peter Moss <peter@moss.dk>
2019-10-14 20:19:19 +0200

    Autori

---
+++ Autori/csharp/app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split("-");
+            var res = "";
+            foreach (var s in input)
+            {
+                res += s[0];
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
