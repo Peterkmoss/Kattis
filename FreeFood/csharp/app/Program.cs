10ff417ef6740653442da4994faefba2f0eb96b3
Peter K. Moss <peter@moss.dk>
2019-10-14 23:11:26 +0200

    Free Food

---
+++ FreeFood/csharp/app/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var events = int.Parse(Console.ReadLine());
+            var days = new HashSet<int>();
+            var latest = 0;
+            for (var i = 0; i < events; i++)
+            {
+                var e = Console.ReadLine().Split(" ");
+                var start = int.Parse(e[0]);
+                var end = int.Parse(e[1]);
+
+                for (var j = start; j <= end; j++)
+                {
+                    days.Add(j);
+                }
+            }
+            Console.WriteLine(days.Count);
+        }
+    }
+}
