a19d9e05661d5b49827ac45afa13fb9a8144448b
Peter K. Moss <peter@moss.dk>
2019-10-14 22:52:29 +0200

    Aaah

---
+++ Aaah/csharp/app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var max = Console.ReadLine().Where(c => c == 'a').Count();
+            if (Console.ReadLine().Where(c => c == 'a').Count() > max)
+                Console.WriteLine("no");
+            else
+                Console.WriteLine("go");
+
+        }
+    }
+}
