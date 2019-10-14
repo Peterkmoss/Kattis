3834aa7b172123a115e26955747a309f1cd458ae
Peter Moss <peter@moss.dk>
2019-10-14 20:35:45 +0200

    Job Expenses

--- JobExpenses/csharp/app/Program.cs
+++ JobExpenses/csharp/app/Program.cs
@@ -6,7 +6,16 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var n = int.Parse(Console.ReadLine());
+            var res = 0;
+            var numbers = Console.ReadLine().Split(" ");
+            for (var i = 0; i < n; i++)
+            {
+                var number = int.Parse(numbers[i]);
+                if (number < 0)
+                    res += Math.Abs(number);
+            }
+            Console.WriteLine(res);
         }
     }
 }
