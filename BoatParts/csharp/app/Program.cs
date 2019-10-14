6fb5a3ef816f12300b8bf927989f0c4069656b2d
Peter K. Moss <peter@moss.dk>
2019-10-14 22:47:19 +0200

    Boat Parts

---
+++ BoatParts/csharp/app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ");
+            var set = new HashSet<string>();
+            for (var i = 0; i < int.Parse(input[1]); i++)
+            {
+                set.Add(Console.ReadLine());
+                if (set.Count == int.Parse(input[0]))
+                {
+                    Console.WriteLine(i+1);
+                    return;
+                }
+            }
+            Console.WriteLine("paradox avoided");
+        }
+    }
+}
---
+++ PizzaHawaii-TODO/csharp/app/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace app
+{
+    class Program
+    {
+
+        private static IDictionary<string, string> dictionary = new Dictionary<string, string>();
+
+        static void Main(string[] args)
+        {
+            var testCases = int.Parse(Console.ReadLine());
+            for (var i = 0; i < testCases; i++)
+            {
+                parseMenu(dictionary);
+
+                Console.WriteLine();
+            }
+        }
+
+        static void parseMenu(IDictionary<string, string> dictionary)
+        {
+            var pizzas = int.Parse(Console.ReadLine());
+            for (var i = 0; i < pizzas; i++)
+            {
+                parsePizza(dictionary);
+            }
+        }
+
+        static void parsePizza(IDictionary<string, string> dictionary)
+        {
+            var pizza = Console.ReadLine();
+            var native = Console.ReadLine().Split(" ");
+            var english = Console.ReadLine().Split(" ");
+
+            for (var i = 1; i <= Math.Max(int.Parse(native[0]), int.Parse(native[0])); i++)
+            {
+                if (i <= english.Length)
+                {
+                    dictionary.Add(pizza, english[i]);
+                }
+                if (i <= native.Length)
+                {
+                    dictionary.Add(pizza, native[i]);
+                }
+            }
+        }
+    }
+}
