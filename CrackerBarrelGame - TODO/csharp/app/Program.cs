1a0c1684c3c40c6b85f11b94bb3675bea5275c7c
Peter Moss <peter@moss.dk>
2019-10-18 11:32:39 +0200

    Encoded Message

---
+++ CrackerBarrelGame - TODO/csharp/app/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks.Dataflow;
+using System.Text.RegularExpressions;
+using System;
+using static System.Console;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exp = @"\S[2]";
+            var targetPin = ReadLine().Trim();
+            var first = ReadLine().ToCharArray();
+            var second = ReadLine().ToCharArray();
+            var third = ReadLine().ToCharArray();
+            var fourth = ReadLine().ToCharArray();
+            var fifth = ReadLine().ToCharArray();
+
+
+        }
+    }
+}
---
+++ EncodedMessage/csharp/app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cases = int.Parse(ReadLine());
+            for (var i = 0; i < cases; i++)
+            {
+                var input = ReadLine().ToCharArray();
+                var size = (int)Math.Sqrt(input.Length);
+                for (var j = size - 1; j >= 0; j--)
+                {
+                    for (var k = j; k < input.Length; k += size)
+                        Write(input[k]);
+                }
+                WriteLine();
+            }
+        }
+    }
+}
