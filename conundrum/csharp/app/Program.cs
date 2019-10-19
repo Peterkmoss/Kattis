f72916c51827e44f141d96ed04e0e15adcfff904
Peter Moss <peter@moss.dk>
2019-10-19 23:35:44 +0200

    conundrum

--- conundrum/csharp/app/Program.cs
+++ conundrum/csharp/app/Program.cs
@@ -6,7 +6,15 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = Console.ReadLine();
+            var days = 0;
+            for (var i = 0; i < input.Length; i += 3)
+            {
+                if (input[i] != 'P') days++;
+                if (input[i + 1] != 'E') days++;
+                if (input[i + 2] != 'R') days++;
+            }
+            Console.WriteLine(days);
         }
     }
 }
