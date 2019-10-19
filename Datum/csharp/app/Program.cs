263a1ed5776ef57645ceda24e64cf039af71b3cc
Peter Moss <peter@moss.dk>
2019-10-19 23:29:13 +0200

    Datum

---
+++ Datum/csharp/app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Array.ConvertAll(ReadLine().Split(" "), s => int.Parse(s));
+            var date = new DateTime(2009, input[1], input[0]);
+            WriteLine(date.DayOfWeek);
+        }
+    }
+}
