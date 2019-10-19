49b4ca57a5491c94b8ea50a6a89ca6bbe808099d
Peter Moss <peter@moss.dk>
2019-10-19 23:23:19 +0200

    apaxiaaans

---
+++ apaxiaaans/csharp/app/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = ReadLine();
+            var result = "";
+            char last = ' ';
+
+            foreach (var c in input)
+            {
+                if (c != last)
+                {
+                    result += c;
+                    last = c;
+                }
+            }
+            WriteLine(result);
+        }
+    }
+}
