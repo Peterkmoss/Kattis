da11c603dfd65594e76390c70629adaacd3b954f
Peter Moss <peter@moss.dk>
2019-10-19 15:00:12 +0200

    Anthony and Diablo

---
+++ AnthonyAndDiablo/csharp/app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Array.ConvertAll(ReadLine().Split(" "), s => float.Parse(s));
+            if (Math.Pow(input[1] / (2 * Math.PI), 2) * Math.PI >= input[0])
+                WriteLine("Diablo is happy!");
+            else
+                WriteLine("Need more materials!");
+        }
+    }
+}
