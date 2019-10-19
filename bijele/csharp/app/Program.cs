e073548d97402875ca867f3606158cc5eb106dd5
Peter Moss <peter@moss.dk>
2019-10-19 23:42:32 +0200

    bijele

---
+++ bijele/csharp/app/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Array.ConvertAll(ReadLine().Split(" "), s => int.Parse(s));
+            Write(1 - input[0] + " ");
+            Write(1 - input[1] + " ");
+            Write(2 - input[2] + " ");
+            Write(2 - input[3] + " ");
+            Write(2 - input[4] + " ");
+            Write(8 - input[5]);
+        }
+    }
+}
