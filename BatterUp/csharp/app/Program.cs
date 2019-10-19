22f195737b4a8e99d267a9a9a81ccf3e211ffb4a
Peter Moss <peter@moss.dk>
2019-10-19 23:12:24 +0200

    Batter up

---
+++ BatterUp/csharp/app/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadLine();
+            var input = Array.ConvertAll(ReadLine().Split(" "), s => int.Parse(s));
+            var res = 0.0;
+            var count = 0;
+            foreach (var i in input)
+            {
+                if (i >= 0)
+                {
+                    res += i;
+                    count++;
+                }
+            }
+            res /= count;
+            WriteLine(res);
+        }
+    }
+}
