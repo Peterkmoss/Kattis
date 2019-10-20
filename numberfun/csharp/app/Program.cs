179b2e914d12e7290a3024b991e0b7e2f9a08d4e
Peter Moss <peter@moss.dk>
2019-10-20 11:31:46 +0200

    Number fun

---
+++ numberfun/csharp/app/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cases = int.Parse(ReadLine());
+            for (var i = 0; i < cases; i++)
+            {
+                var input = Array.ConvertAll(ReadLine().Split(" "), s => int.Parse(s));
+                var a = input[0];
+                var b = input[1];
+                var c = input[2];
+                if (a + b == c
+                    || a - b == c
+                    || a * b == c
+                    || (float)a / (float)b == c
+                    || b - a == c
+                    || (float)b / (float)a == c)
+                    WriteLine("Possible");
+                else
+                    WriteLine("Impossible");
+            }
+        }
+    }
+}
