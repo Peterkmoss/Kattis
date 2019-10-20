9ffd299bcd27c42688e6ceb7c8e190212fa4164e
Peter Moss <peter@moss.dk>
2019-10-20 11:44:08 +0200

    zamka

--- zamka/csharp/app/Program.cs
+++ zamka/csharp/app/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace app
 {
@@ -6,7 +7,38 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var L = int.Parse(ReadLine());
+            var D = int.Parse(ReadLine());
+            var X = int.Parse(ReadLine());
+
+            for (var n = L; n <= D; n++)
+            {
+                if (findNumber(n, X) != 0)
+                {
+                    WriteLine(n);
+                    break;
+                }
+            }
+            for (var m = D; m >= L; m--)
+            {
+                if (findNumber(m, X) != 0)
+                {
+                    WriteLine(m);
+                    break;
+                }
+            }
+        }
+
+        private static int findNumber(int i, int x)
+        {
+            var iString = i.ToString();
+            var number = 0;
+            foreach (var c in iString)
+                number += int.Parse(c.ToString());
+            if (number == x)
+                return number;
+            else
+                return 0;
         }
     }
 }
