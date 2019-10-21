b97b38c6b21ae73708f23f5fe0827762ff33c0a9
Peter Moss <peter@moss.dk>
2019-10-21 10:12:50 +0200

    Identifying map tiles

---
+++ maptiles2/csharp/app/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System;
+using static System.Console;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = ReadLine();
+            var zoom = input.Length;
+            var x = 0;
+            var y = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var c = input[zoom - i - 1];
+                var increase = (int)Math.Pow(2, i + 1) / 2;
+                if (c == '1') x += increase;
+                else if (c == '2') y += increase;
+                else if (c == '3')
+                {
+                    x += increase;
+                    y += increase;
+                }
+            }
+
+            WriteLine($"{zoom} {x} {y}");
+        }
+    }
+}
