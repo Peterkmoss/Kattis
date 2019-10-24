9e5d893fc39d044387c55b18a30c8a5af10e1cfe
Peter Moss <peter@moss.dk>
2019-10-24 17:33:29 +0200

    Janitortroubles started

---
+++ janitortroubles/csharp/janitortroubles/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace janitortroubles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Array.ConvertAll(Console.ReadLine().Split(" "), s => int.Parse(s));
+            var a = input[0];
+            var b = input[1];
+            var c = input[2];
+            var d = input[3];
+
+            var lort = (a + b + c + d) / 2;
+
+            Console.WriteLine(Math.Sqrt((lort - a) * (lort - b) * (lort - c) * (lort - d)));
+        }
+    }
+}
