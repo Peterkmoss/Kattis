76e16f2db745dcae60c3ab182a04ecc63a9b7b55
Peter Moss <peter@moss.dk>
2019-10-24 18:51:22 +0200

    Pot

---
+++ pot/csharp/pot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace pot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tests = int.Parse(ReadLine());
+            var result = 0.0;
+            for (int i = 0; i < tests; i++)
+            {
+                var input = ReadLine();
+                var arr = input.ToCharArray();
+                var pow = int.Parse(arr[arr.Length - 1].ToString());
+                var str = "";
+                for (int j = 0; j < arr.Length - 1; j++)
+                    str += arr[j];
+                var num = int.Parse(str);
+                result += Math.Pow(num, pow);
+
+            }
+            WriteLine(result);
+        }
+    }
+}
